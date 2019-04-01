9e3dc4bb08f01e5edd863f10647a2149d45cecd4
jbolanosc <josue.carit@outlook.com>
2019-04-01 08:52:01 -0600

    h
    module 1

---
+++ src/Quiz.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Component
+} from 'react';
+
+var quizData = require('./quiz_data.json');
+
+class Quiz extends Component {
+
+  constructor(props) {
+    super(props);
+    this.setState = {
+      quiz_position : 1
+    }
+  }
+
+  render() {
+    return (
+      <div><div className = "QuizQuestion" >{quizData.quiz_questions[0].instruction_text}</div></div>
+    )
+
+  }
+
+
+}
+
+export default Quiz;
